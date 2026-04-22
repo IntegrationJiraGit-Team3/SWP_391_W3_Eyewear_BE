e3db2ede9e1db31492818470c6a7aed81169b155
RyanNCD <danh.11x2.nguyen@gmail.com>
2026-04-23 00:28:09 +0700

    update productive inventory report response

--- src/main/java/com/fpt/glasseshop/entity/dto/response/ProductInventoryReportResponse.java
+++ src/main/java/com/fpt/glasseshop/entity/dto/response/ProductInventoryReportResponse.java
@@ -11,8 +11,14 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ProductInventoryReportResponse {
     private String productName;
+    private String productType;
     private String color;
     private Long purchasedQuantity;
     private Long inStockQuantity;
     private Long preorderQuantity;
+
+    private java.math.BigDecimal revenue;
+    private java.math.BigDecimal completedRevenue;
+    private java.math.BigDecimal pendingRevenue;
+    private java.math.BigDecimal refundedRevenue;
 }
